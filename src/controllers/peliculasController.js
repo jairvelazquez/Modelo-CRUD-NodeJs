f8f77496092d7e72046af8e43d2929d9fcc21ca5
LuisAbraham <lago9797@gmail.com>
2020-05-27 17:54:47 -0500

    si 2

--- src/controllers/peliculasController.js
+++ src/controllers/peliculasController.js
@@ -2,16 +2,57 @@ const controller = {};
 
 controller.list = (req, res) => {
     req.getConnection((err, conn) => {
-        conn.quey('select * from pelicula',(err, peliculas) => {
+        conn.query('select * from pelicula',(err, rows) => {
             if(err){
                 res.json(err);
             }
 
             res.render('peliculasView', {
-                data: peliculas
+                data: pelicula
             });
         });
     });
+};
+
+controller.save = (req, res) =>{
+    const data = req.body;
+
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO peliculas set ?', [data], (err,rows) => {
+            res.redirect('/');
+        });
+    })
+};
+
+controller.edit =(req, res) => {
+    const {id} = req.params;
+
+    req.getConnection((err, conn) =>{
+        conn.query('SELECT * FROM peliculas WHERE id ?', [id], (err, row));
+        res.render('peliculas_edit', {
+            data: pelicula
+        });
+    });
+};
+
+
+controller.update = (req, res) => {
+    const {id} = req.params;
+    const newPelicula = req.body;
+    req.getConnection((err,conn) => {
+        conn.query('UPDATE pelicula set ? WHERE id = ?', [newPelicula, id], (err, row) => {
+            res.redirect('/');
+        });
+    });
+};
+
+controller.delete = (req, res) => {
+    const {id} = req.params;
+    req.getConnection((err, conn) => {
+        conn.query('DELETE FROM peliculas WHERE id = ?', [id], (err, rows) => {
+            res.redirect('/');
+        });
+    }) 
 };
 
 module.exports = controller;
--- src/router/peliculaRout.js
+++ src/router/peliculaRout.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const peliculasController = require('../controllers/peliculasController');
 
- router.get('/', peliculasController.list);
+router.get('/', peliculasController.list);
+router.post('/add', peliculasController.save);
+router.get('/delete/:id', peliculasController.delete);
 
+router.get('/update/:id', peliculasController.edit);
+router.post('/update/:id', peliculasController.update);
  module.exports = router;
