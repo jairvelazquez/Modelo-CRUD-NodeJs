e9fd64f95d2d1a3e7ec0acab2a6fb784b623c367
roylando <65689744+roylando@users.noreply.github.com>
2020-05-28 18:07:51 -0500

    Cambios a presentacion
    Agrega vista de personalidad

--- src/controllers/personalidadController.js
+++ src/controllers/personalidadController.js
@@ -1,23 +1,63 @@
 const controller = {};
 
+
 controller.list = (req, res) => {
+  const query = "select * from personalidad";
   req.getConnection((err, conn) => {
-    conn.query('SELECT * FROM personalidad', (err, personalidad) => {
+    conn.query(query, (err, query) => {
      if (err) {
       res.json(err);
      }
      res.render('personalidades', {
-        data: personalidad
+        data: query
      });
     });
   });
 };
 
-controller.save = (req, res) => {
-  const data = req.body;
+
+controller.list1 = (req, res) => {
+  let q1 = " select personalidad.id, personalidad.nombre, personalidad.apellidos, participa.rol, participa.id_pelicula, pelicula.nombre_p"
+  let q2 = " from personalidad inner join participa on personalidad.id = participa.id_personalidad inner join pelicula on participa.id_pelicula = pelicula.id"
+  let q3 = " order by personalidad.id;"
+  const query = q1.concat(q2,q3);
+  req.getConnection((err, conn) => {
+    conn.query(query, (err, query) => {
+     if (err) {
+      res.json(err);
+     }
+     res.render('person_part', {
+        data: query
+     });
+    });
+  });
+};
+
+controller.proc1 = (req, res) => {
+  const data1 = req.body;
   console.log(req.body)
   req.getConnection((err, connection) => {
-    const query = connection.query('INSERT INTO personalidad set ?', data, (err, personalidad) => {
+    const query = connection.query('call agregar_personalidad_guionista(?,?,?,?,?,?)', [data1.nom,data1.ap,data1.sexo,data1.nacim,data1.gui,data1.gen], (err, personalidad) => {
+      console.log(personalidad)
+      res.redirect('/personalidad');
+    })
+  })
+};
+controller.proc2 = (req, res) => {
+  const data2 = req.body;
+  console.log(req.body)
+  req.getConnection((err, connection) => {
+    const query = connection.query('call agregar_personalidad_director(?,?,?,?,?,?,?)', [data2.nom,data2.ap,data2.sexo,data2.nacim,data2.genP,data2.est,data2.peliR], (err, personalidad) => {
+      console.log(personalidad)
+      res.redirect('/personalidad');
+    })
+  })
+};
+controller.proc3 = (req, res) => {
+  const data3 = req.body;
+  console.log(req.body)
+  req.getConnection((err, connection) => {
+    const query = connection.query('call agregar_personalidad_actor(?,?,?,?,?,?)', [data3.nom,data3.ap,data3.sexo,data3.nacim,data3.orig,data3.arq], (err, personalidad) => {
       console.log(personalidad)
       res.redirect('/personalidad');
     })
@@ -54,16 +94,5 @@ controller.delete = (req, res) => {
     });
   });
 }
-
-controller.proc1 = (req, res) => {
-  const data1 = req.body;
-  console.log(req.body)
-  req.getConnection((err, connection) => {
-    const query = connection.query('call agregar_personalidad_guionista(?,?,?,?,?,?)', [data1.nom,data1.ap,data1.sexo,data1.nacim,data1.gui,data1.gen], (err, personalidad) => {
-      console.log(personalidad)
-      res.redirect('/personalidad');
-    })
-  })
-};
 
 module.exports = controller;
